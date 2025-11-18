411162696660558742aca5e9bcf6534f1fdad99d
鈴木 <suzukik@analix.co.jp>
2025-11-18 13:46:48 +0900

    refactor: 複雑なバリデーションを CreateScheduleRequest に移行し、新規作成及び更新メソッドを簡素化

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
 use App\Models\Movie;
 use App\Models\Genre;
 use App\Models\Schedule;
-use Illuminate\Validation\ValidationException;
+use App\Http\Requests\CreateScheduleRequest;
 
 class MovieController extends Controller
 {
@@ -158,72 +159,11 @@ class MovieController extends Controller
         return view('admin.schedules.edit', compact('movie', 'schedule'));
     }
 
-    public function scheduleUpdate(Request $request, $id) {
-        $schedule = Schedule::findOrFail($id);
+    public function scheduleUpdate(CreateScheduleRequest $request, $id) {
+          $schedule = Schedule::findOrFail($id);
 
-        $validated = $request->validate([
-            'movie_id' => ['exists:movies,id'],
-            'start_time_date' => ['required', 'date_format:Y-m-d', 'before_or_equal:end_time_date'],
-            'start_time_time' => ['required', 'date_format:H:i', 'before:end_time_time',
-                function($attribute, $value, $fail) {
-                    // 組み立て
-                    $startStr = request()->input('start_time_date') . ' ' . request()->input('start_time_time');
-                    $endStr   = request()->input('end_time_date')   . ' ' . request()->input('end_time_time');
+        $validated = $request->validated();
 
-                    // try/catch で Carbon を安全に扱う
-                    try {
-                        $start = CarbonImmutable::createFromFormat('Y-m-d H:i', $startStr);
-                        $end   = CarbonImmutable::createFromFormat('Y-m-d H:i', $endStr);
-                    } catch (\Exception $e) {
-                        $fail('開始/終了の日時形式が正しくありません');
-                        return;
-                    }
-
-                    // 差分チェック
-                    if ($start->diffInMinutes($end) < 6) {
-                        $fail('開始時刻と終了時刻の差は5分以上にしてください');
-                    }
-                },
-            ],
-            'end_time_date' => ['required', 'date_format:Y-m-d', 'after_or_equal:start_time_date'],
-            'end_time_time' => ['required', 'date_format:H:i', 'after:start_time_time', 
-                function($attribute, $value, $fail) {
-                    // 組み立て
-                    $startStr = request()->input('start_time_date') . ' ' . request()->input('start_time_time');
-                    $endStr   = request()->input('end_time_date')   . ' ' . request()->input('end_time_time');
-
-                    // try/catch で Carbon を安全に扱う
-                    try {
-                        $start = CarbonImmutable::createFromFormat('Y-m-d H:i', $startStr);
-                        $end   = CarbonImmutable::createFromFormat('Y-m-d H:i', $endStr);
-                    } catch (\Exception $e) {
-                        $fail('開始/終了の日時形式が正しくありません');
-                        return;
-                    }
-
-                    // 差分チェック
-                    if ($start->diffInMinutes($end) < 6) {
-                        $fail('開始時刻と終了時刻の差は5分以上にしてください');
-                    }
-                },
-            ]
-        ],[
-            'movie_id.exists' => '指定された映画が存在しません',
-            'start_time_date.required' => '開始日の入力は必須です',
-            'start_time_date.date_format' => '開始日は有効な日付形式で入力してください',
-            'start_time_time.before_or_equal' => '開始時間は終了時間より前の時間にしてください',
-            'start_time_time.required' => '開始時間の入力は必須です',
-            'start_time_time.date_format' => '開始時間は有効な時間形式で入力してください',
-            'end_time_date.required' => '終了日の入力は必須です',
-            'end_time_date.date_format' => '終了日は有効な日付形式で入力してください',
-            'end_time_time.required' => '終了時間の入力は必須です',
-            'start_time_date.before' => '終了日時は開始日時より後にしてください',
-            'start_time_time.before' => '終了日時は開始日時より後にしてください',
-            'end_time_date.after_or_equal' => '開始時間は終了時間より前の時間にしてください',
-            'end_time_time.after' => '開始時間は終了時間より前の時間にしてください',
-        ]);
-
-        
         $start = Carbon::createFromFormat('Y-m-d H:i', $validated['start_time_date'] . ' ' . $validated['start_time_time']);
         $end   = Carbon::createFromFormat('Y-m-d H:i', $validated['end_time_date']   . ' ' . $validated['end_time_time']);
                         
@@ -243,71 +183,11 @@ class MovieController extends Controller
         return view('admin.schedules.create', compact('movie'));
     }
 
-    public function scheduleStore(Request $request, $id) {
-        $movie = Movie::findOrFail($id);
+    public function scheduleStore(CreateScheduleRequest $request, $id) {
+         $movie = Movie::findOrFail($id);
 
-        $validated = $request->validate([
-            'movie_id' => ['exists:movies,id'],
-            'start_time_date' => ['required', 'date_format:Y-m-d', 'before_or_equal:end_time_date'],
-            'start_time_time' => ['required', 'date_format:H:i', 'before:end_time_time', 
-                function($attribute, $value, $fail) {
-                    // 組み立て
-                    $startStr = request()->input('start_time_date') . ' ' . request()->input('start_time_time');
-                    $endStr   = request()->input('end_time_date')   . ' ' . request()->input('end_time_time');
+        $validated = $request->validated();
 
-                    // try/catch で Carbon を安全に扱う
-                    try {
-                        $start = CarbonImmutable::createFromFormat('Y-m-d H:i', $startStr);
-                        $end   = CarbonImmutable::createFromFormat('Y-m-d H:i', $endStr);
-                    } catch (\Exception $e) {
-                        $fail('開始/終了の日時形式が正しくありません');
-                        return;
-                    }
-
-                    // 差分チェック
-                    if ($start->diffInMinutes($end) < 6) {
-                        $fail('開始時刻と終了時刻の差は5分以上にしてください');
-                    }
-                },
-            ],
-            'end_time_date' => ['required', 'date_format:Y-m-d', 'after_or_equal:start_time_date'],
-            'end_time_time' => ['required', 'date_format:H:i', 'after:start_time_time', 
-                function($attribute, $value, $fail) {
-                    // 組み立て
-                    $startStr = request()->input('start_time_date') . ' ' . request()->input('start_time_time');
-                    $endStr   = request()->input('end_time_date')   . ' ' . request()->input('end_time_time');
-
-                    // try/catch で Carbon を安全に扱う
-                    try {
-                        $start = CarbonImmutable::createFromFormat('Y-m-d H:i', $startStr);
-                        $end   = CarbonImmutable::createFromFormat('Y-m-d H:i', $endStr);
-                    } catch (\Exception $e) {
-                        $fail('開始/終了の日時形式が正しくありません');
-                        return;
-                    }
-
-                    // 差分チェック
-                    if ($start->diffInMinutes($end) < 6) {
-                        $fail('開始時刻と終了時刻の差は5分以上にしてください');
-                    }
-                },
-            ],
-        ],[
-            'movie_id.exists' => '指定された映画が存在しません',
-            'start_time_date.required' => '開始日の入力は必須です',
-            'start_time_date.date_format' => '開始日は有効な日付形式で入力してください',
-            'start_time_time.before_or_equal' => '開始時間は終了時間より前の時間にしてください',
-            'start_time_time.required' => '開始時間の入力は必須です',
-            'start_time_time.date_format' => '開始時間は有効な時間形式で入力してください',
-            'end_time_date.required' => '終了日の入力は必須です',
-            'end_time_date.date_format' => '終了日は有効な日付形式で入力してください',
-            'end_time_time.required' => '終了時間の入力は必須です',
-            'start_time_date.before' => '終了日時は開始日時より後にしてください',
-            'start_time_time.before' => '終了日時は開始日時より後にしてください',
-            'end_time_date.after_or_equal' => '開始時間は終了時間より前の時間にしてください',
-            'end_time_time.after' => '開始時間は終了時間より前の時間にしてください',
-        ]);
-        
         $start = Carbon::createFromFormat('Y-m-d H:i', $validated['start_time_date'] . ' ' . $validated['start_time_time']);
         $end   = Carbon::createFromFormat('Y-m-d H:i', $validated['end_time_date']   . ' ' . $validated['end_time_time']);
     
--- app/Http/Requests/CreateScheduleRequest.php
+++ app/Http/Requests/CreateScheduleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Carbon\CarbonImmutable;
 
 class CreateScheduleRequest extends FormRequest
 {
@@ -24,11 +25,61 @@ class CreateScheduleRequest extends FormRequest
     public function rules()
     {
         return [
-            'movie_id' => ['required'],
+            // ルートパラメータから movie_id を取る場合もあるので柔軟に
+            'movie_id' => ['sometimes', 'exists:movies,id'],
             'start_time_date' => ['required', 'date_format:Y-m-d', 'before_or_equal:end_time_date'],
-            'start_time_time' => ['required', 'date_format:H:i'],
             'end_time_date' => ['required', 'date_format:Y-m-d', 'after_or_equal:start_time_date'],
-            'end_time_time' => ['required', 'date_format:H:i'],
+            'start_time_time' => ['required', 'date_format:H:i','before:end_time_time',
+                function($attribute, $value, $fail) {
+                    $startStr = $this->input('start_time_date') . ' ' . $this->input('start_time_time');
+                    $endStr   = $this->input('end_time_date')   . ' ' . $this->input('end_time_time');
+
+                    $this->validateInterval($startStr, $endStr, $fail);
+                },
+            ],
+            'end_time_time' => ['required', 'date_format:H:i', 'after:start_time_time',
+                function($attribute, $value, $fail) {
+                    $startStr = $this->input('start_time_date') . ' ' . $this->input('start_time_time');
+                    $endStr   = $this->input('end_time_date')   . ' ' . $this->input('end_time_time');
+
+                    $this->validateInterval($startStr, $endStr, $fail);
+                },
+            ],
         ];
+    }
+    public function messages(): array
+    {
+        return [
+            'movie_id.exists' => '指定された映画が存在しません',
+            'start_time_date.required' => '開始日の入力は必須です',
+            'start_time_date.date_format' => '開始日は有効な日付形式で入力してください',
+            'start_time_time.before_or_equal' => '開始時間は終了時間より前の時間にしてください',
+            'start_time_time.required' => '開始時間の入力は必須です',
+            'start_time_time.date_format' => '開始時間は有効な時間形式で入力してください',
+            'end_time_date.required' => '終了日の入力は必須です',
+            'end_time_date.date_format' => '終了日は有効な日付形式で入力してください',
+            'end_time_time.required' => '終了時間の入力は必須です',
+            'start_time_date.before' => '終了日時は開始日時より後にしてください',
+            'start_time_time.before' => '終了日時は開始日時より後にしてください',
+            'end_time_date.after_or_equal' => '開始時間は終了時間より前の時間にしてください',
+            'end_time_time.after' => '開始時間は終了時間より前の時間にしてください',
+        ];
+    }
+
+    // 日時差分チェック
+     private function validateInterval(string $startStr, string $endStr, $fail): void
+    {
+        try {
+            $start = CarbonImmutable::createFromFormat('Y-m-d H:i', $startStr);
+            $end   = CarbonImmutable::createFromFormat('Y-m-d H:i', $endStr);
+        } catch (\Exception $e) {
+            $fail('開始/終了の日時形式が正しくありません');
+            return;
+        }
+
+        if ($start->diffInMinutes($end) < 6) {
+            $fail('開始時刻と終了時刻の差は5分以上にしてください');
+            return;
+        }
     }
 }
